324aeefe7e1acbe3ae8f4e2e4b637fab5e0e1609
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2024-08-07 17:04:01 -0700

    changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Welcome to Bizza Platform!'
+    return 'Welcome To Bizza Platform!'
 
 
 if __name__ == '__main__':
